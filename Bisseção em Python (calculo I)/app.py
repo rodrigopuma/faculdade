981145eba946fd19907d93aa1e666e3ab96417fa
aluno <aluno0@unicap.br>
2025-09-04 16:48:43 -0300

    Introduzindo a atividade de calcular bisseção com python

---
+++ Bisseção em Python (calculo I)/app.py
@@ -0,0 +1,50 @@
+def transformar_funcao(var, func: str):
+    var = str(var)
+    func = func.replace('x', var)
+    return func
+
+def calcular_funcao_str(x: float, f_x: str):
+    def calcular_str_como_expressao(x= x, f_x= f_x):
+        f_x = eval(f_x)
+        return f_x
+    
+    f_x = calcular_str_como_expressao()
+    return f_x
+
+def calcular_bissecao(a: float,
+                      b: float,
+                      FX: str):
+    """Calcula a bisseção da função baseado no intervalo do ponto a ao ponto b
+    A multiplicação dos 2 tem de resultar em um número negativo
+
+    Args:
+        a (float): Ponto a
+        b (float): Ponto b
+    """
+    ax = str(a) # transformando o x em a
+    bx = str(b)
+    str_f_a = transformar_funcao(ax, FX) # str(fx) # criando uma função para transformar x em a
+    str_f_b = transformar_funcao(bx, FX)
+    imagem_a = calcular_funcao_str(ax, str_f_a) # float(f_a)
+    imagem_b = calcular_funcao_str(bx, str_f_b) # float(f_b)
+
+    if imagem_a * imagem_b > 0:
+        return "Não é possível calcular a raiz dessa função, pelo método da bisseção."
+    else:
+        k = 0 # criterio de parada
+        media_xk = (a + b) / 2 # media de A e B
+
+        while not k / media_xk < 0.01:
+            if k == 10: # teste
+                break # teste
+            media_xk = (a + b) / 2
+            str_f_xk = transformar_funcao(media_xk, FX)
+            imagem_media_xk = calcular_funcao_str(media_xk, str_f_xk)
+
+            if imagem_a * imagem_media_xk < 0: # então: os sinais são diferentes
+                b = media_xk # logo b <- xk
+            elif imagem_b * imagem_media_xk < 0: # então: os sinais são diferentes
+                a = media_xk
+            k += 1
+        return media_xk 
+            
---
+++ Bisseção em Python (calculo I)/main.py
@@ -0,0 +1,24 @@
+from app import calcular_bissecao
+
+def main():
+    a = float(input('Digite o primeiro ponto do intervalo: '))
+    b = float(input('Digite o segundo ponto do intervalo: '))
+    f_x = input('Qual vai ser sua função (Se quiser ver o guia de operadores digite guia): ').lower().strip()
+    if f_x == 'guia':
+        print('''
+[+]  = Soma
+[-]  = Subtração
+[*]  = Multiplicação
+[/]  = Divisão
+[//] = Divisão Inteira
+[%]  = Módulo (Resto da divisão)
+[**] = Exponenciação
+[()] = Ordem de Precedência
+''')
+        f_x = input('Agora digite a sua função baseado nesses operadores e sem o f(x) (Apenas a expressão): ')
+    calcular_bissecao(a, b, f_x)
+
+if __name__ == '__main__':
+    run = main()
+
+print(run)
