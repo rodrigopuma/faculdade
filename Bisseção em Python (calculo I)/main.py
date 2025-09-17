11aa7db0f07156e3dc37e51e098071f09ec90cd6
Rodrigo Puma <99513585+rodrigopuma@users.noreply.github.com>
2025-09-17 17:37:30 -0300

    Update main.py

--- Bisseção em Python (calculo I)/main.py
+++ Bisseção em Python (calculo I)/main.py
@@ -3,6 +3,7 @@ from app import calcular_bissecao
 def main():
     a = float(input('Digite o primeiro ponto do intervalo: '))
     b = float(input('Digite o segundo ponto do intervalo: '))
+    precisao = float(input("Qual será o critério de parada em um número decimal: ").replace(',','.'))
     f_x = input('Qual vai ser sua função (Se quiser ver o guia de operadores digite guia): ').lower().strip()
     if f_x == 'guia':
         print('''
@@ -16,9 +17,9 @@ def main():
 [()] = Ordem de Precedência
 ''')
         f_x = input('Agora digite a sua função baseado nesses operadores e sem o f(x) (Apenas a expressão): ')
-    calcular_bissecao(a, b, f_x)
+    bissecao = calcular_bissecao(a, b, f_x, precisao)
+    return bissecao
 
 if __name__ == '__main__':
     run = main()
-
-print(run)
+    print(run)
